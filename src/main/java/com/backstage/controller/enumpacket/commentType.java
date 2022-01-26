6f5954d30524d41181c3d3f6713c32a9f9353447
Olive-2019 <1497250390@qq.com>
2022-01-26 14:08:53 +0800

    边栏整理完成

---
+++ src/main/java/com/backstage/controller/enumpacket/commentType.java
@@ -0,0 +1,5 @@
+package com.backstage.controller.enumpacket;
+
+public enum commentType{
+    comment_To_Video, comment_To_Comment
+}
---
+++ src/main/java/com/backstage/controller/enumpacket/likeType.java
@@ -0,0 +1,5 @@
+package com.backstage.controller.enumpacket;
+
+public enum likeType{
+    like_Video,like_Comment
+}
